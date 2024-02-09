be56d7bc7189b24a5c55280b163afde763ed40d3
Ákos Kiss <ak@coram.pub>
2024-02-09 18:00:36 +0100

    Add solution for day 12 part two

--- 12.lisp
+++ 12.lisp
@@ -2,37 +2,57 @@
 
 (defun day12 (is-part-two)
   (let* ((lines (uiop:read-file-lines #P"./12.txt"))
-         (rows (loop :for line :in lines
-                     :for (springs groups) := (ppcre:split " " line)
-                     :collect (cons springs
-                                    (mapcar #'parse-integer
-                                            (ppcre:split "," groups))))))
-    (labels ((replace-char (s i c)
-               (let ((s2 (copy-seq s)))
-                 (setf (char s2 i) c)
-                 s2))
-             (variations (springs)
-               (loop :with variations := (list springs)
+         (rows (labels ((unfold (separator xs)
+                          (apply #'concatenate 'string
+                                 (butlast (mapcan (lambda (x)
+                                                    (list x separator))
+                                                  (loop :repeat 5
+                                                        :collect xs))))))
+                 (loop :for line :in lines
+                       :for (springs groups) := (ppcre:split " " line)
+                       :for unfolded-springs := (if is-part-two
+                                                    (unfold "?" springs)
+                                                    springs)
+                       :for unfolded-groups := (if is-part-two
+                                                   (unfold "," groups)
+                                                   groups)
+                       :collect
+                       (cons (ppcre:regex-replace-all
+                              "\\.+" (format nil "~A." unfolded-springs) ".")
+                             (mapcar #'parse-integer
+                                     (ppcre:split "," unfolded-groups))))))
+         (cache (make-hash-table :test 'equal)))
+    (labels ((valid-subseqs (springs length)
+               (loop :with n := 0 :and first-broken-index := nil
                      :for c :across springs :for i :from 0
-                     :when (char= c #\?)
-                       :do (setf variations
-                                 (loop :for v :in variations
-                                       :collect (replace-char v i #\.)
-                                       :collect (replace-char v i #\#)))
-                     :finally (return variations)))
-             (is-valid (variation groups)
-               (equal groups
-                      (loop :with n := 0 :and results := nil
-                            :for c :across variation
-                            :if (char= c #\#)
-                              :do (incf n)
-                            :else :when (plusp n)
-                                    :do (push n results) :and :do (setf n 0)
-                            :finally (return (nreverse (if (plusp n)
-                                                           (cons n results)
-                                                           results)))))))
-      (if is-part-two
-          nil
-          (loop :for (springs . groups) :in rows
-                :sum (count-if (lambda (v) (is-valid v groups))
-                               (variations springs)))))))
+                     :for next := (ignore-errors (char springs (1+ i)))
+                     :do (ecase c
+                           (#\. (setf n 0))
+                           (#\# (unless first-broken-index
+                                  (setf first-broken-index i))
+                            (incf n))
+                           (#\? (incf n)))
+                     :while (or (not first-broken-index)
+                                (< (- i length) first-broken-index))
+                     :when (and (<= length n) (member next '(#\. #\?)))
+                       :collect (subseq springs
+                                        (+ i 1 (if (char= next #\?) 1 0)))
+                         :into results
+                     :finally (return (setf (gethash (cons springs length)
+                                                     cache)
+                                            results))))
+             (combinations (springs groups)
+               (multiple-value-bind (c is-hit) (gethash (cons springs groups)
+                                                        cache)
+                 (if is-hit
+                     c
+                     (setf (gethash (cons springs groups) cache)
+                           (if (cdr groups)
+                               (loop :for v :in (valid-subseqs springs
+                                                               (car groups))
+                                     :sum (combinations v (cdr groups)))
+                               (loop :for v :in (valid-subseqs springs
+                                                               (car groups))
+                                     :count (not (find #\# v)))))))))
+      (loop :for (springs . groups) :in rows
+            :sum (combinations springs groups)))))
