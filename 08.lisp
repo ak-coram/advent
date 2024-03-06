827457a54cb722996a4ebe015140047058577937
Ákos Kiss <ak@coram.pub>
2024-03-06 07:30:47 +0100

    Tweak solution for day 8

--- 08.lisp
+++ 08.lisp
@@ -2,46 +2,38 @@
 (ql:quickload :cl-ppcre)
 
 (defun day08 (is-part-two)
-  (destructuring-bind (instructions nodes start-nodes end-nodes)
-      (loop :named outer
-            :with (instructions . network) := (uiop:read-file-lines "./08.txt")
-            :for node-definition :in network
-            :for node := (ppcre:register-groups-bind (a b c)
-                             ("(\\w+) = \\((\\w+), (\\w+)\\)" node-definition)
-                           (cons (intern a :keyword)
-                                 (cons (intern b :keyword)
-                                       (intern c :keyword))))
-            :when node
-              :collect node :into nodes
-            :finally
-               (loop :for node :in (mapcar #'car nodes)
-                     :for last-char := (char (symbol-name node) 2)
-                     :when (eql last-char #\A)
-                       :collect node :into start-nodes
-                     :when (eql last-char #\Z)
-                       :collect node :into end-nodes
-                     :finally (return-from outer
-                                (list (loop :for i :across instructions
-                                            :collect (intern (format nil "~C" i)
-                                                             :keyword))
-                                      nodes
-                                      (if is-part-two
-                                          start-nodes
-                                          (list :AAA))
-                                      (if is-part-two
-                                          end-nodes
-                                          (list :ZZZ))))))
-    (loop :with infinite-instructions := (setf (cdr (last instructions)) instructions)
-          :for start-node :in start-nodes
-          :collect (loop :with node := start-node
-                         :for i :in infinite-instructions
-                         :for (l . r) := (alexandria:assoc-value nodes node)
-                         :do (setf node (case i
-                                          (:L l)
-                                          (:R r)))
-                         :count t :into step-count
-                         :when (member node end-nodes)
-                           :return step-count)
-            :into cycles
-          :finally (return (apply #'lcm cycles)))))
-
+  (let* ((lines (uiop:read-file-lines "./08.txt"))
+         (instructions (car lines))
+         (nodes (mapcar (lambda (node-definition)
+                          (ppcre:register-groups-bind (a b c)
+                              ("(\\w+) = \\((\\w+), (\\w+)\\)" node-definition)
+                            (cons (intern a :keyword)
+                                  (cons (intern b :keyword)
+                                        (intern c :keyword)))))
+                        (cddr lines))))
+    (destructuring-bind (instructions nodes start-nodes end-nodes)
+        (loop :for (node . nil) :in nodes
+              :for last-char := (char (symbol-name node) 2)
+              :when (eql last-char #\A)
+                :collect node :into start-nodes
+              :when (eql last-char #\Z)
+                :collect node :into end-nodes
+              :finally (return (list (loop :for i :across instructions
+                                           :collect (intern (format nil "~C" i)
+                                                            :keyword))
+                                     nodes
+                                     (if is-part-two start-nodes (list :AAA))
+                                     (if is-part-two end-nodes (list :ZZZ)))))
+      (loop :for start-node :in start-nodes
+            :collect (loop :with node := start-node
+                           :for i :in (apply #'alexandria:circular-list
+                                             instructions)
+                           :for (l . r) := (alexandria:assoc-value nodes node)
+                           :do (setf node (case i
+                                            (:L l)
+                                            (:R r)))
+                           :count t :into step-count
+                           :when (member node end-nodes)
+                             :return step-count)
+              :into cycles
+            :finally (return (apply #'lcm cycles))))))
