2100cc5a85c57854399599f4670673d0388be249
Ákos Kiss <ak@coram.pub>
2024-09-22 16:15:03 +0200

    2015/07 Use integers instead of bit-vectors for wire state

--- 2015/07.lisp
+++ 2015/07.lisp
@@ -3,8 +3,7 @@
 (use-package :esrap)
 
 (defrule signal (+ (digit-char-p character))
-  (:lambda (digits)
-    (parse-integer (text digits))))
+  (:lambda (digits) (parse-integer (text digits))))
 
 (defrule unary-operator "NOT" (:constant 'not))
 
@@ -12,18 +11,13 @@
   (:lambda (op) (intern (text op))))
 
 (defrule wire (+ (alphanumericp character))
-  (:lambda (w)
-    (intern (text w))))
+  (:lambda (w) (intern (text w))))
 
 (defrule fixed-signal (and signal " -> " wire)
-  (:destructure (s _ w)
-    (declare (ignore _))
-    (list 'fix w s)))
+  (:destructure (s _ w) (declare (ignore _)) (list 'fix w s)))
 
 (defrule negation (and unary-operator #\space wire " -> " wire)
-  (:destructure (_1 _2 s _3 d)
-    (declare (ignore _1 _2 _3))
-    (list 'not d s)))
+  (:destructure (_1 _2 s _3 d) (declare (ignore _1 _2 _3)) (list 'not d s)))
 
 (defrule operation (and (or signal wire)
                         #\space binary-operator #\space
@@ -33,61 +27,44 @@
     (list op d s1 s2)))
 
 (defrule assignment (and wire " -> " wire)
-  (:destructure (s _ d)
-    (declare (ignore _))
-    (list 'assign d s)))
+  (:destructure (s _ d) (declare (ignore _)) (list 'assign d s)))
 
-(defrule instruction
-    (or fixed-signal negation operation assignment))
+(defrule instruction (or fixed-signal negation operation assignment))
+
+(defconstant +max-value+ (1- (expt 2 16)))
 
 (defun day07 (is-part-two)
-  (labels ((integer-to-bits (i)
-             (loop :with bits := (make-array '(16) :element-type 'bit)
-                   :for bit-index :below 16
-                   :do (setf (aref bits bit-index)
-                             (ldb (byte 1 bit-index) i))
-                   :finally (return bits)))
-           (bits-to-integer (bits)
-             (loop :with i := 0
-                   :for bit-index :below 16 
-                   :do (setf (ldb (byte 1 bit-index) i) (aref bits bit-index))
-                   :finally (return i)))
-           (iterate (state instructions)
+  (labels ((iterate (state instructions)
              (loop :for instruction :in instructions
                    :for (op d s1 s2) := instruction
                    :unless (gethash d state)
                      :do (ecase op
-                           (fix (setf (gethash d state) (integer-to-bits s1)))
+                           (fix (setf (gethash d state) s1))
                            (assign (alexandria:when-let (v (gethash s1 state))
                                      (setf (gethash d state) v)))
                            (not (alexandria:when-let (v (gethash s1 state))
-                                  (setf (gethash d state) (bit-not v))))
+                                  (setf (gethash d state) (- +max-value+ v))))
                            ((and or)
                             (alexandria:when-let
-                                ((a (if (integerp s1)
-                                        (integer-to-bits s1)
-                                        (gethash s1 state)))
-                                 (b (if (integerp s2)
-                                        (integer-to-bits s2)
-                                        (gethash s2 state))))
+                                ((a (if (integerp s1) s1 (gethash s1 state)))
+                                 (b (if (integerp s2) s2 (gethash s2 state))))
                               (setf (gethash d state)
                                     (ecase op
-                                      (and (bit-and a b))
-                                      (or (bit-ior a b))))))
+                                      (and (logand a b))
+                                      (or (logior a b))))))
                            ((rshift lshift)
                             (alexandria:when-let (v (gethash s1 state))
                               (setf (gethash d state)
-                                    (integer-to-bits
-                                     (ecase op
-                                       (rshift (ash (bits-to-integer v) (- s2)))
-                                       (lshift (ash (bits-to-integer v) s2))))))))
+                                    (mod (ash v (ecase op
+                                                  (rshift (- s2))
+                                                  (lshift s2)))
+                                         +max-value+)))))
                      :and :collect instruction))
            (solve (instructions)
              (loop :with state := (make-hash-table)
                    :do (setf instructions (iterate state instructions))
                    :while instructions
-                   :finally (return (bits-to-integer (gethash (intern "a")
-                                                              state))))))
+                   :finally (return (gethash (intern "a") state)))))
     (let* ((instructions (loop :for line :in (uiop:read-file-lines #P"./07.txt")
                                :collect (parse 'instruction line)))
            (part-one-solution (solve instructions)))
