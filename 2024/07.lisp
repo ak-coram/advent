e8e18cc7f5c6ab5b7a76f1dd5c37ca3db600d0bc
Ákos Kiss <ak@coram.pub>
2024-12-07 06:53:36 +0100

    Simplify parsing

--- 2024/07.lisp
+++ 2024/07.lisp
@@ -1,26 +1,18 @@
 (defun day07 (is-part-two)
-  (let ((equations 
-          (loop :for line :in (uiop:read-file-lines #P"./07.txt")
-                :for (test-input numbers-input)
-                  := (uiop:split-string line :separator '(#\:))
-                :collect
-                (cons (parse-integer test-input)
-                      (loop :for (x offset)
-                              := (multiple-value-list
-                                  (parse-integer numbers-input
-                                                 :start (or offset 0)
-                                                 :junk-allowed t))
-                            :while x :collect x)))))
-    (labels ((|| (x y) (+ (* (expt 10 (floor (1+ (log y 10)))) x) y))
-             (possiblep (equation)
-               (loop :with (test-value . numbers) := equation
-                     :for x :in numbers
-                     :for accs := (list x)
-                       :then (loop :for acc :in accs
-                                   :collect (* acc x) :collect (+ acc x)
-                                   :when is-part-two
-                                     :collect (|| acc x))
-                     :finally (return (member test-value accs)))))
-      (loop :for equation :in equations
-            :when (possiblep equation)
-              :sum (car equation)))))
+  (labels ((|| (x y) (+ (* (expt 10 (floor (1+ (log y 10)))) x) y))
+           (possiblep (equation)
+             (loop :with (test-value . numbers) := equation
+                   :for x :in numbers
+                   :for accs := (list x)
+                     :then (loop :for acc :in accs
+                                 :collect (* acc x) :collect (+ acc x)
+                                 :when is-part-two
+                                   :collect (|| acc x))
+                   :finally (return (member test-value accs)))))
+    (loop
+      :for line :in (uiop:read-file-lines #P"./07.txt")
+      :for equation
+        := (loop :for s :in (uiop:split-string line :separator '(#\: #\Space))
+                 :for n := (parse-integer s :junk-allowed t) :when n :collect n)
+      :when (possiblep equation)
+        :sum (car equation))))
